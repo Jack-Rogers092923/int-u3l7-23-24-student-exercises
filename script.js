6480d23aa4560863fe32b9b09cde4a33f8733642
Jack-Rogers092923 <jackrogers9918@gmail.com>
2025-06-11 14:31:52 -0500

    Done

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // 1. Declare a variable called myName. 
 //  - Give it the value of your name. 
 //  - This is a string so the value should be in quotes ""
-
+let myName = "Jack"; // Replace with your name
 
 
 // 2. Declare a variable called age. 
 //  - Give it the value of your age. 
 //  - This is a number, so the value does not have to be in quotes
-
+let age = 17; // Replace with your age
 // 3. Declare a variable called answer.
 //  - This should save the div with a class of `answer`
-
+let answer = document.querySelector(".answer"); // This selects the div with class "answer"
 
 // 4. When you're done, remove the comment from the code below.
 
-// answer.innerHTML = myName + " is " + age;
+answer.innerHTML = myName + " is " + age;
